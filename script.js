09cc6c10b26e69deb006b2a1eebc568d491b652e
CopsLikeDonuts <reggisen@gmail.com>
2020-12-11 17:56:32 +0200

    started to develop logic

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-window.addEventListener('')
+const listNames = ['My workday', 'Work', 'Buy food'];
+const listContent = document.querySelector('.dropdown-content');
+const listDisplayBtn = document.getElementById('dropbtn');
+
+
+function renderLists() {
+    while (listContent[0]) {
+        listContent.lastElementChild.remove()
+    };
+// removeChild(select.lastChild)
+    
+    for (let i = 0; i < listNames.length; i++) {
+    
+        let newListItem = document.createElement('span');
+        newListItem.classList.add('dropdown-content');
+        newListItem.innerText = listNames[i];
+        listDisplayBtn.append(newListItem);
+    }
+}
+renderLists();
+
+
+const addListForm = document.getElementById('list-add-form');
+const addListInput = document.getElementById('list-add-input');
+addListForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    listNames.push(addListInput.value);
+    renderLists();
+
+})
+
