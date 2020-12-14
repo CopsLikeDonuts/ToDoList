ee38aa6952782f2f26f7d877c115875d83f214fc
CopsLikeDonuts <reggisen@gmail.com>
2020-12-14 20:17:22 +0200

    rendering lists

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-const listNames = ['My workday', 'Work', 'Buy food'];
+document.addEventListener('DOMContentLoaded', () => {
+// const listNames = ['My workday', 'Work', 'Buy food'];
+const listNames = {
+    'My workday': ['Answer e-mails', 'Meeting', 'Code', 'Study content', 'Report'],
+    'My morning': ['Get up', 'Clean teeth', 'Have a breakfast', 'Take a laptop'],
+    'Buy food': ['Apples', 'Oranges', 'Potatoes', 'Milk', 'Chicken fillet', 'Coffee', 'Cheese']
+};
 const listContent = document.querySelector('.dropdown-content');
 const listDisplayBtn = document.getElementById('dropbtn');
 
-
+//render toDoLists
 function renderLists() {
-    while (listContent[0]) {
-        listContent.lastElementChild.remove()
-    };
-// removeChild(select.lastChild)
+    listContent.innerHTML = '';
     
-    for (let i = 0; i < listNames.length; i++) {
-    
+    for (let i = 0; i < Object.keys(listNames).length; i++) {
         let newListItem = document.createElement('span');
-        newListItem.classList.add('dropdown-content');
-        newListItem.innerText = listNames[i];
+        newListItem.classList.add('dropdown-content', 'lists');
+        newListItem.innerText = Object.keys(listNames)[i];
         listDisplayBtn.append(newListItem);
     }
 }
 renderLists();
 
-
+//add new ToDo list
 const addListForm = document.getElementById('list-add-form');
 const addListInput = document.getElementById('list-add-input');
 addListForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    listNames.push(addListInput.value);
+    listNames[addListInput.value] = [];
     renderLists();
+    displayLists();
+    addListForm.reset();
+});
 
-})
+//display list properities
+function displayLists() {
+    const toDoLists = document.querySelectorAll('.lists');
+    const listName = document.getElementById('list-name');
+    toDoLists.forEach(item => {
+        item.addEventListener('click', () => {
+            listName.innerText = item.innerText;
+        });
+    });
+}
+displayLists();
+
+});
 
---
+++ test.js
@@ -0,0 +1,7 @@
+const listNames = {
+    'My workday': ['Answer e-mails', 'Meeting', 'Code', 'Study content', 'Report'],
+    'My morning': ['Get up', 'Clean teeth', 'Have a breakfast', 'Take a laptop'],
+    'Buy food': ['Apples', 'Oranges', 'Potatoes', 'Milk', 'Chicken fillet', 'Coffee', 'Cheese']
+};
+console.log(Object.keys(listNames).length);
+console.log(Object.keys(listNames)[0]);
