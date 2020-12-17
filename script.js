11843e4315a8f13f0e2064ee114e7087e7edfe84
CopsLikeDonuts <reggisen@gmail.com>
2020-12-17 15:41:07 +0200

    continue develop logic

--- script.js
+++ script.js
@@ -39,8 +39,7 @@ function displayLists() {
     toDoLists.forEach(item => {
         item.addEventListener('click', (e) => {
             listName.innerText = item.innerText;
-            renderTasks(listNames, e.target.innerText)
-            
+            renderTasks(listNames, e.target.innerText);
         });
     });
 }
@@ -49,7 +48,6 @@ displayLists();
 });
 
 function renderTasks(object, value) {
-    debugger;
     const taskList = document.getElementById('task-list');
     taskList.innerHTML = ''
     for (let i = 0; i < object[value].length; i++) {
@@ -60,6 +58,34 @@ function renderTasks(object, value) {
         <input class ='task-delete' type="submit" value='-'>
         `
         taskList.appendChild(div);
+        markTasks();
     }
+}
 
+// add task
+const taskAddForm = document.getElementById('task-add-form');
+const taskAddInput = document.getElementById('task-add-input');
+const taskList = document.getElementById('task-list');
+taskAddForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let newItem = document.createElement('div');
+    newItem.innerText = taskAddInput.value;
+
+    taskList.appendChild(`
+    <input class ='task-select' type='radio'>
+        ${newItem}
+    <input class ='task-delete' type="submit" value='-'>
+    `);
+});
+// mark tasks as completed
+
+function markTasks() {
+    const tasksSelector = document.querySelectorAll('.task-select');
+
+    tasksSelector.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            e.target.disabled = true;
+            e.target.nextElementSibling.style.cssText = 'text-decoration: line-through';
+        });
+    });
 }
